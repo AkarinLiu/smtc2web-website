6566655e24c58f336a0f429d214159640b23869c
AkarinLiu <akarinliu@outlook.com>
2026-09-10 12:43:32 +0800

    添加 matomo 跟踪代码

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -8,6 +8,16 @@ export default {
   extends: DefaultTheme,
   Layout: CustomLayout,
   enhanceApp({ app, router, siteData }) {
-    // ...
+    if (typeof window === "undefined") return;
+    const _paq = (window._paq = window._paq || []);
+    _paq.push(["enableLinkTracking"]);
+    _paq.push(["setTrackerUrl", "https://analytics.akarinliu.com/matomo.php"]);
+    _paq.push(["setSiteId", "5"]);
+    _paq.push(["trackPageView"]);
+    const g = document.createElement("script");
+    g.async = true;
+    g.src = "https://analytics.akarinliu.com/matomo.js";
+    document.head.appendChild(g);
+    router.onAfterRouteChanged = () => _paq.push(["trackPageView"]);
   }
 } satisfies Theme
